ba3b49941209025b18fca45bbd7a7997eacaa9f3
Rahul Bavaliya <41228277+RahulBavaliya@users.noreply.github.com>
2025-10-08 11:30:15 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
+const isVercel = process.env.VERCEL === '1'; // true when on Vercel
+const isGitHubPages = process.env.GITHUB_PAGES === 'true'; // we’ll set this in the GitHub workflow
+const repoName = 'rsatokengenerator';
+
 const nextConfig = {
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  basePath: isVercel ? '' : `/${repoName}`,
+  assetPrefix: isVercel ? '' : `/${repoName}/`,
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
